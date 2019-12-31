ee234ca228f55f7d1534cee887ba43fff8c62d31
Kunal Singh <ks1697@gmail.com>
2020-01-01 03:23:17 +0530

    adding capstone notebook project to repository

---
+++ Capstone_Notebook_Project.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assignment - Capstone Notebook Project
+# 
+
+# This notebook is for the assignment of Coursera course Applied Data Science Capstone
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
